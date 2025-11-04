6a5413672e1d741c7e04745fcf276ee50891f7fd
Davis Samael Brenes Montiel <164054709+DSBM06@users.noreply.github.com>
2025-11-04 08:48:32 -0600

    Mensaje

--- src/main/java/ni/edu/uam/Facturacion/Entidades/ICalculator.java
+++ src/main/java/ni/edu/uam/Facturacion/Entidades/ICalculator.java
@@ -1,4 +1,16 @@
 package ni.edu.uam.Facturacion.Entidades;
 
+import ni.edu.uam.Facturacion.Calculadores.CalculadorSiguienteNumeroParaAnyo;
+import org.openxava.annotations.DefaultValueCalculator;
+import org.openxava.annotations.PropertyValue;
+
+import javax.persistence.Column;
+
 public class ICalculator {
+    @Column(length=6)
+    @DefaultValueCalculator(value= CalculadorSiguienteNumeroParaAnyo.class,
+            properties=@PropertyValue(name="anyo") // Para inyectar el valor de anyo de Factura
+            // en el calculador antes de llamar a calculate()
+    )
+    int numero;
 }
--- src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
+++ src/main/java/ni/edu/uam/Facturacion/modelo/Factura.java
@@ -1,4 +1,52 @@
 package ni.edu.uam.Facturacion.modelo;
 
+import java.time.*;
+import java.util.Collection;
+import javax.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+import lombok.*;
+
+@Entity @Getter @Setter
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones"
+)
 public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
+    int anyo;
+
+    @Column(length=6)
+    int numero;
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=false) // El cliente es obligatorio
+    @ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
+    Cliente cliente;
+
+    @ElementCollection
+    @ListProperties("producto.numero, producto.descripcion, cantidad")
+    Collection<Detalle> detalles;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @DescriptionsList
+    Autor autor;
 }
